4bfdcd9690c99af175c45a440c6292b40f5a97a2
nurRiyad <asadnurriyad@gmail.com>
2023-02-04 13:43:49 +0600

    add tailwindcss, vueuse, pinia, nuxt-img, nuxt-robot, typescript
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Some default layout shared across all pages</h1>
+    <slot />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-5xl">
+    Home Page
+  </div>
+</template>
