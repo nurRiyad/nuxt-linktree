11ab0bc2dc248ee359dcc28b4e36d714fbf941b8
Al Asad Nur Riyad <asadnurriyad@gmail.com>
2023-02-25 17:19:01 +0600

    Add collection options
    Signed-off-by: Al Asad Nur Riyad <asadnurriyad@gmail.com>

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -10,7 +10,7 @@ const props = withDefaults(defineProps<Props>(), {
 
 const superClient = useSupabaseClient<Database>()
 
-const { data: collection, pending, refresh } = await useLazyAsyncData('get-collection', async () => {
+const { data: collection, pending, refresh: reFetchCollection } = await useLazyAsyncData('get-collection', async () => {
   const { data, error } = await superClient
     .from('collections')
     .select('user_name, collection_name, description')
@@ -21,6 +21,30 @@ const { data: collection, pending, refresh } = await useLazyAsyncData('get-colle
 
   return data
 })
+
+const showModal = ref(false)
+const collectinName = ref('')
+const collectionDes = ref('')
+const isCreating = ref(false)
+
+const onAddClick = async () => {
+  try {
+    isCreating.value = true
+    const { error } = await superClient
+      .from('collections')
+      .insert([
+        { user_name: props.username, collection_name: collectinName.value, description: collectinName.value },
+      ])
+    if (!error) {
+      reFetchCollection()
+      showModal.value = false
+    }
+  }
+  catch (error) {
+    console.log(error)
+  }
+  isCreating.value = false
+}
 </script>
 
 <template>
@@ -29,13 +53,55 @@ const { data: collection, pending, refresh } = await useLazyAsyncData('get-colle
       <p class="text-lg md:text-2xl  lg:text-5xl font-bold">
         Top Ten Collection
       </p>
-      <button class="btn btn-sm md:btn-md  btn-primary">
-        Create New
+      <button
+        class="btn btn-sm md:btn-md  btn-primary" @click="showModal = !showModal"
+      >
+        Create
       </button>
     </div>
     <div class="w-full mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5">
       <div v-for="c in collection" :key="c.collection_name">
         <DashCard :collection-name="c.collection_name" :user-name="c.user_name" :description="c.description" />
+      </div>
+    </div>
+    <!-- Put this part before </body> tag -->
+    <div class="modal" :class="{ 'modal-open': showModal }">
+      <div class="modal-box">
+        <div class="flex justify-between">
+          <h3 class="font-bold text-lg">
+            Create New Task
+          </h3>
+          <button
+            class="btn btn-circle btn-primary btn-sm btn-outline"
+            @click="showModal = false"
+          >
+            <LogosClose />
+          </button>
+        </div>
+
+        <div class="py-4 flex flex-col space-y-3">
+          <input
+            v-model="collectinName"
+            type="text"
+            placeholder="Collection Name"
+            class="input input-bordered input-primary w-full"
+          >
+          <input
+            v-model="collectionDes"
+            type="text"
+            placeholder="Collection Desscription"
+            class="input input-bordered input-primary w-full"
+          >
+        </div>
+        <div class="flex justify-end">
+          <button
+            class="btn btn-primary"
+            :class="{ loading: isCreating }"
+            @click="onAddClick"
+          >
+            Button
+          </button>
+        </div>
       </div>
     </div>
   </div>
