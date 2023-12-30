6ac878e32ce70628a85a456639d9ddd95adc1647
nurRiyad <asadnurriyad@gmail.com>
2023-12-31 01:31:57 +0600

    Add public page
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

---
+++ components/PublicItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <ULink to="/" class="hover:shadow-lg cursor-pointer">
+    <UCard>
+      <p class="text-center">
+        Facebook
+      </p>
+    </UCard>
+  </ULink>
+</template>
---
+++ pages/usr/[username]/[collection].vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="mt-10 flex flex-col justify-center">
+      <UAvatar
+        size="3xl"
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+        class="mx-auto"
+      />
+      <div class="flex flex-col items-center py-5">
+        <h2 class="text-2xl font-semibold">
+          AL Asad Nur Riyad
+        </h2>
+        <p>Software Engineer</p>
+      </div>
+    </div>
+    <div class="grid gap-4 my-4">
+      <PublicItem />
+      <PublicItem />
+      <PublicItem />
+      <PublicItem />
+      <PublicItem />
+      <PublicItem />
+      <PublicItem />
+    </div>
+  </div>
+</template>
