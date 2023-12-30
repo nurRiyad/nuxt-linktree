ba6461d0a2891d284de0947df97686bb1ad9801b
nurRiyad <asadnurriyad@gmail.com>
2023-12-31 01:46:32 +0600

    Add 404 page
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

---
+++ pages/[...slug].vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-full flex justify-center items-center">
+    <div>
+      <h1 class="text-6xl font-semibold text-red-400">
+        404
+      </h1>
+      <p>Page not found</p>
+    </div>
+  </div>
+</template>
