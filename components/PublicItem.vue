227d538bc2db6f6c9393c5a900d462d99ec5f600
nurRiyad <asadnurriyad@gmail.com>
2024-01-06 12:13:11 +0600

    Add public page design
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- components/PublicItem.vue
+++ components/PublicItem.vue
@@ -5,9 +5,12 @@
 <template>
   <ULink to="/" class="hover:shadow-lg cursor-pointer">
     <UCard>
-      <p class="text-center">
-        Facebook
-      </p>
+      <div class="flex justify-center items-center space-x-3">
+        <p class="text-center">
+          Facebook
+        </p>
+        <Icon name="line-md:external-link-rounded" size="18" />
+      </div>
     </UCard>
   </ULink>
 </template>
